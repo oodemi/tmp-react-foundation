967c1938bf343f73a6d0ef10e7c710863ddb4281
hidemi.atarashi <hidemi@coworker.co.jp>
2024-08-22 21:50:02 +0900

    スレッド作成画面のスタイルを調整

--- src/ThreadsCreate/ThreadsCreate.tsx
+++ src/ThreadsCreate/ThreadsCreate.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import "./threadsCreate.css";
 import Header from "../Header/Header";
 
@@ -10,50 +10,61 @@ export default function ThreadsCreate() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const postData = {
-      title: createTitle
-    }
+      title: createTitle,
+    };
 
     try {
       await fetch("https://railway.bulletinboard.techtrain.dev/threads", {
         method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(postData),
       })
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
           alert("スレッドの作成完了:" + data.title);
           setCreateTitle("");
           navigate("/");
-        })
+        });
+    } catch (error) {
+      console.log("スレッドが作成できませんでした。", error);
     }
-    catch (error) {
-      console.log("スレッドが作成できませんでした。", error)
-    }
-  }
+  };
 
   return (
     <>
       <Header>
-        <Link to="/" className='threadCreateLink' >Topに戻る</Link>
+        <Link to="/" className="threadCreateLink">
+          Topに戻る
+        </Link>
       </Header>
-      <section>
-        <h1>スレッド新規作成</h1>
-        <label>
+      <section className="container">
+        <h1 className="pageTitle">Create Thread</h1>
+        <div className="threadCreateForm">
+
+        <label htmlFor="threadTitle" className="threadTitleInputLabel">
           スレッドタイトル
-          <input type="text"
-            value={createTitle}
-            name="threadTitle"
-            onChange={(e) => setCreateTitle(e.target.value)}
-            placeholder="タイトルを入力してください"
-          />
         </label>
+        <input
+          type="text"
+          value={createTitle}
+          name="threadTitle"
+          id="threadTitle"
+          onChange={(e) => setCreateTitle(e.target.value)}
+          placeholder="タイトルを入力してください"
+          className="threadTitleInput"
+        />
         <button
+          className="createButton"
           onClick={handleSubmit}
           disabled={createTitle === ""}
-        >作成</button>
+        >
+          作成
+        </button>
+        </div>
+
       </section>
     </>
-  )
+  );
 }
